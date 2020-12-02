6f8270ba98595bf0fdd3f82f391155cddf585f06
Jess <jmedved85@gmail.com>
2020-12-02 12:35:36 +0100

    Riješen zadatak 5.2.8 - Prijestupna godina

---
+++ 5.2.8 Prijestupna godina/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5._2._8_Prijestupna_godina
+{
+    class Program
+    {
+        static void Main()
+        {
+            int godina = 0;
+            try
+            {
+                Console.Write("Unesite godinu: ");
+                godina = int.Parse(Console.ReadLine());
+            }
+            catch 
+            {
+                Console.WriteLine("Pogrešan unos.");
+                return;
+            }
+            if (((godina % 4 == 0) && (!(godina % 100 == 0))) || (godina % 400 == 0))
+            {
+                Console.WriteLine("{0} JEST prijestupna godina.", godina);
+            }
+            else
+            {
+                Console.WriteLine("{0} NIJE prijestupna godina.", godina);
+            }
+            Console.ReadKey();
+        }
+    }
+}
